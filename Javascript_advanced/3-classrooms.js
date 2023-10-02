39819fa4853a554ab5b139d00b179118d5bb7bc6
unknown <mahmoudhannani3@gmail.com>
2023-10-02 09:39:00 +0100

    students_seat

---
+++ Javascript_advanced/3-classrooms.js
@@ -0,0 +1,18 @@
+function createClassRoom(numberOfStudents) {
+    function studentSeat(seat) {
+      return function() {
+        return seat;
+      }
+    }
+    
+    var students = [];
+    
+    for (var i = 0; i < numberOfStudents; i++) {
+      students.push(studentSeat(i + 1));
+    }
+    
+    return students;
+  }
+  
+  var classRoom = createClassRoom(10);
+  

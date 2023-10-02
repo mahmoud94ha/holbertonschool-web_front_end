db89ebaee5991a4c0f9c7de57d0e73daa404a80b
unknown <mahmoudhannani3@gmail.com>
2023-10-02 16:00:51 +0100

    multi mod

---
+++ Javascript_advanced/100-stock.js
@@ -0,0 +1,33 @@
+let stock = {
+    macbook: 2,
+    iphone: 4
+  };
+  
+  function processPayment(itemName) {
+    stock[itemName.toLowerCase()] -= 1;
+    console.log(`Payment is being processed for item ${itemName}`);
+  }
+  
+  function processError(itemName) {
+    console.log(`No more ${itemName} in stock`);
+    console.log("Payment is not being processed");
+  }
+  
+  function processOrder(itemName, callbackPayment, callbackError) {
+    console.log(`Verifying the stock of ${itemName}`);
+    
+    if (stock.hasOwnProperty(itemName.toLowerCase()) && stock[itemName.toLowerCase()] > 0) {
+      callbackPayment(itemName);
+    } else {
+      callbackError(itemName);
+    }
+  }
+  
+  let item = prompt("Please enter the item you would like to purchase (Macbook, iPhone):");
+  item = item.toLowerCase();
+  
+  if (stock.hasOwnProperty(item)) {
+    processOrder(item, processPayment, processError);
+  } else {
+    processError(item);
+  }
